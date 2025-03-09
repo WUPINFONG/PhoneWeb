a42019bcda14558d118ee8c72053581855dcf648
wu <perry11514@gmail.com>
2025-03-09 12:02:45 +0800

    設定

--- phone web/src/App.jsx
+++ phone web/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 const [user,setUser]=useState(null);
   return(<>
     <Navbar></Navbar>
-    <BrowserRouter basename='/phone-web-'>
+    <BrowserRouter basename='/phone-web'>
     
       <Routes>
           <Route path="/" element={<ShareLaout/>} />
