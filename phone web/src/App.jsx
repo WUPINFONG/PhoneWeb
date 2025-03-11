9b2bf7c13e31a205e2dc2a71e7df2a7180bd2687
wu <perry11514@gmail.com>
2025-03-11 23:08:08 +0800

    app.jsx

--- phone web/src/App.jsx
+++ phone web/src/App.jsx
@@ -1,11 +1,12 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react"; 
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Home";
 import Shopping from "./Shopping";
 import PhoneLease from "./PhoneLease";
 import Contactus from "./Contactus";
 import ShareLaout from "./ShareLaout";
-import SingleProduct from "./SingleProduct";
+import SingleProduct from "./SingleProduct"
 import ShoppingIphone from "./ShoppingIphone";
 import ShoppingIpad from "./ShoppingIpad";
 import ShoppingSamsung from "./ShoppingSamsung";
@@ -21,35 +22,47 @@ import Login from "./Login";
 import Dashboard from "./Dashboard";
 
 function App() {
-  const [user, setUser] = useState(null);
 
-  return (
-    <>
-      <Navbar />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<ShareLaout />} />
-          <Route index element={<Home />} />
+const [user,setUser]=useState(null);
+  return(<>
+    <Navbar></Navbar>
+    
+    <BrowserRouter basename='/PhoneWeb'>
+    
+      <Routes>
+          <Route path="/" element={<ShareLaout/>} />
+          <Route index element={<Home/>}/>
           <Route path="/shopping" element={<Shopping />} />
-          <Route path="/shopping/iphone" element={<ShoppingIphone />} />
-          <Route path="/shopping/ipad" element={<ShoppingIpad />} />
-          <Route path="/shopping/samsung" element={<ShoppingSamsung />} />
-          <Route path="/shopping/xiaomi" element={<ShoppingXiaomi />} />
-          <Route path="/shopping-list/:productId" element={<SingleProduct />} />
-          <Route path="/phonelease" element={<PhoneLease />} />
-          <Route path="/phonelease/iphone" element={<PhoneLeaseIphone />} />
-          <Route path="/phonelease/ipad" element={<PhoneLeaseIpad />} />
-          <Route path="/phonelease/samsung" element={<PhoneLeaseSamsung />} />
-          <Route path="/phonelease/xiaomi" element={<PhoneLeaseXiaomi />} />
-          <Route path="/phonelease-list/:leaseId" element={<SingleLease />} />
-          <Route path="/contact-us" element={<Contactus />} />
-          <Route path="/login" element={<Login setUser={setUser} />} />
-          <Route path="/dashboard" element={<Dashboard user={user} />} />
-        </Routes>
-      </HashRouter>
-      <Footer />
+          
+          
+          <Route path="shopping/iphone" element={<ShoppingIphone/>}/>
+          <Route path="shopping/ipad" element={<ShoppingIpad/>}/>
+          <Route path="shopping/samsung" element={<ShoppingSamsung/>}/>
+          <Route path="shopping/Xiaomi" element={<ShoppingXiaomi/>}/>    
+          <Route path="/shopping-list/:productId" element={<SingleProduct/>}/>  
+
+          <Route path='phonelease' element={<PhoneLease/>} />
+          <Route path="phonelease/iphone" element={<PhoneLeaseIphone/>}/>
+          <Route path="phonelease/ipad" element={<PhoneLeaseIpad/>}/>
+          <Route path="phonelease/samsung" element={<PhoneLeaseSamsung/>}/>
+          <Route path="/phonelease/Xiaomi" element={<PhoneLeaseXiaomi/>}/>
+          
+          <Route path="phonelease-list/:leaseId" element={<SingleLease />} />
+          <Route path='contact-us/' element={<Contactus/>} />
+          <Route path="login" element={<Login setUser={setUser}/>}/>
+          <Route path="dashboard" element={<Dashboard user={user}/>}/>
+         
+         
+        
+        
+        
+      </Routes>
+      
+    </BrowserRouter>
+    <Footer></Footer>
     </>
-  );
-}
+);
+  
+};
 
 export default App;
