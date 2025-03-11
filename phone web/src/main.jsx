92aad668716b9afba41f2177ae6fca453a3516f7
wu <perry11514@gmail.com>
2025-03-11 23:35:38 +0800

    修改

--- phone web/src/main.jsx
+++ phone web/src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client"; // 確保使用 react-dom/client
-import { BrowserRouter } from "react-router-dom"; // 導入 BrowserRouter
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/PhoneWeb"> {/* 這裡加上 BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
-);
+)
