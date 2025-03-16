f88890ffe49792eeb992e375ba55e5b0ad1dd8b3
wu <perry11514@gmail.com>
2025-03-16 10:07:33 +0800

    設定json server

--- phone web/server.js
+++ phone web/server.js
@@ -1,4 +1,5 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -6,8 +7,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-// Render 會動態分配 PORT，所以要用 process.env.PORT
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`✅ JSON Server is running on port ${PORT}`);
+    console.log(`JSON Server is running at http://localhost:${PORT}`);
 });
